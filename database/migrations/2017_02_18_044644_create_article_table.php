882b5a61d5baa4be12f3ba24df9df6f15f89a6e0
weiwenhao <1101140857@qq.com>
2017-03-13 10:55:11 +0800

    直接在article表的迁移文件中修改 publish_at字段增加允许为null,需上线网站需要在数据库工具中修改 publish字段

--- database/migrations/2017_02_18_044644_create_article_table.php
+++ database/migrations/2017_02_18_044644_create_article_table.php
@@ -19,7 +19,7 @@ class CreateArticleTable extends Migration
             $table->string('seo_title')->index()->comment('seo优化标题');
             $table->string('description')->comment('文章描述');
             $table->text('content')->comment('文章内容');
-            $table->timestamp('publish_at')->index()->comment('文章发表时间');
+            $table->timestamp('publish_at')->nullable()->index()->comment('文章发表时间');
             $table->tinyInteger('cat_id')->unsigned()->comment('分类id');
             $table->timestamps();
         });
